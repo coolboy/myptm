fd22487143c6aa47883437962e619745e995f084
coolcute <coolcute@2f7a96df-5cd0-918d-fbe9-3cda5cb4a5af>
2010-04-22 04:05:33 +0000

    参数1： seed, 整数，随机数的种子 参数2： roundrobin， 0或者1， 是否roundrobin还是随机读取 参数3： lines， 整数， 随机读取的行数 参数4：transaction file name . . .
    举例： 10 1 5 sampleShortProcess.txt sampleReadTrx.txt .....

--- myPTM/LockManager.cpp
+++ myPTM/LockManager.cpp
@@ -10,3 +10,13 @@ LockManager::LockManager(void)
 LockManager::~LockManager(void)
 {
 }
+
+LockCondition LockManager::Lock( int tid, int itemid, int type, const std::string& fileName )
+{
+	return LockCondition();
+}
+
+LockCondition LockManager::Lock( const std::string& fileName )
+{
+	return LockCondition();
+}
--- myPTM/Scheduler.cpp
+++ myPTM/Scheduler.cpp
@@ -1,23 +1,60 @@
 #include "StdAfx.h"
+
 #include "Scheduler.h"
+#include "LockManager.h"
 
 #include <iterator>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 using namespace boost;
 
 typedef list<TranManager::Operation> OpLst;
 
-Scheduler::Scheduler(void)
+//////////////////////////////////////////////////////////////////////////
+//Schedule the transactions by the lock
+OpLst ScheduleOperations(const OpLst& ls, LockManager& lm)
 {
-   
+	OpLst ret = ls;
+	BOOST_FOREACH(TranManager::Operation op, ret)
+	{
+		auto tid = op.m0;
+		auto itemid = op.m4;
+		auto type = (int)op.m1;//0 read 1 write 2 delete
+		auto filename = op.m3;
+
+		assert (type != -1);
+		LockCondition lc;
+		if (type != 2)
+			lc = lm.Lock(tid, itemid, type, filename);
+		else
+			lc = lm.Lock(filename);
+
+		if (lc.first == true)//get the lock
+			continue;
+		else {
+			std::vector<int> result = *lc.second;
+			if (result.size() > 1)//dead lock
+				;
+			else
+				;//no dead lock
+		}
+	}
+	return ret;
 }
+//////////////////////////////////////////////////////////////////////////
+
+Scheduler::Scheduler(void){}
 
 void Scheduler::ScheduleTransactions(Operations Transactions)
 {
 	OpLst ls;
+	LockManager lm;
 	copy(Transactions.begin(), Transactions.end(), back_inserter(ls));
+
+	while (ls != ScheduleOperations(ls, lm))
+	{}
 }
 
 Scheduler::~Scheduler(void)
